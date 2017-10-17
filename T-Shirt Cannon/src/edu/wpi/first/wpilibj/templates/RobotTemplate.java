1722826d1d567e9db3430d58c0b62154a7a1331e
Jakob Misbach <squirlemaster@gmail.com>
2017-10-17 19:53:05 -0400

    Started making code more object oriented

--- T-Shirt Cannon/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ T-Shirt Cannon/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,50 +1,61 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class RobotTemplate extends IterativeRobot {
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+    
+    //Drive motors
+    Talon portMaster;
+    Talon portSlave;
+    Talon starMaster;
+    Talon starSlave;
+    
+    //Drive train
+    RobotDrive d;
+    
+    //Joysticks
+    Joystick rs;
+    Joystick ls;
+    
+    //Spikes
+    Relay r5;
+    Relay r6;
+    Relay r7;
+    Relay r8;
+    
     public void robotInit() {
-
-    }
-
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
+        //Drive motors
+        portMaster = new Talon(1);
+        portSlave = new Talon(2);
+        starMaster = new Talon(3);
+        starSlave = new Talon(4);
         
+        //Robot Drive
+        d = new RobotDrive(portMaster, portSlave, starMaster, starSlave);
+        
+        //Joysticks
+        rs = new Joystick(2);
+        ls = new Joystick(1);
+        
+        //Spikes
+        /*
+        *kOff - Turns both relay outputs off
+        *kForward - Sets the relay to forward (M+ @ 12V, M- @ GND)
+        *kReverse - Sets the relay to reverse (M+ @ GND, M- @ 12V)
+        *KOn - Sets both relay outputs on (M+ @ 12V, M- @ 12V).
+        */
+        r5 = new Relay(5);
+        r6 = new Relay(6);
+        r7 = new Relay(7);
+        r8 = new Relay(8);
     }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
+
+    public void teleopPeriodic() {
+        d.tankDrive(ls, rs);
+    }    
 }
