ae62f2a27299b42234fe67527c1df6dca172356b
Jakob Misbach <jakobmisbach8@gmail.com>
2017-11-06 19:33:18 -0500

    Changed var names, updated to use queue, removed state machine

--- T-Shirt Cannon/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ T-Shirt Cannon/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -18,41 +18,37 @@ public class RobotTemplate extends IterativeRobot {
     Talon starSlave;
     
     //Drive train
-    RobotDrive d;
+    RobotDrive driveTrain;
     
     //Joysticks
-    Joystick rs;
-    Joystick ls;
+    Joystick rightStick;
     
     //Spikes
-    Relay r5;
-    Relay r6;
-    Relay r7;
-    Relay r8;
+    Relay relay5;
+    Relay relay6;
+    Relay relay7;
+    Relay relay8;
     
     //Single Relay Side
-    SingleSideSpike s1;
-    SingleSideSpike s2;
-    SingleSideSpike s3;
-    SingleSideSpike s4;
-    SingleSideSpike s5;
-    SingleSideSpike s6;
-    SingleSideSpike s7;
+    SingleSideSpike spike1;
+    SingleSideSpike spike2;
+    SingleSideSpike spike3;
+    SingleSideSpike spike4;
+    SingleSideSpike spike5;
+    SingleSideSpike spike6;
+    SingleSideSpike spike7;
     
     //Barrels
-    Barrel b1;
-    Barrel b2;
-    Barrel b3;
-    Barrel b4;
-    Barrel b5;
-    Barrel b6;
-    Barrel b7;
+    Barrel barrel1;
+    Barrel barrel2;
+    Barrel barrel3;
+    Barrel barrel4;
+    Barrel barrel5;
+    Barrel barrel6;
+    Barrel barrel7;
     
     //Barrel Array
-    Barrel[] barrelList;
-    
-    //State/Index Machine
-    StateMachine stateMachine;
+    CircularQueue barrelList;
     
     //Says if trigger is released
     private boolean released = true;
@@ -65,15 +61,14 @@ public class RobotTemplate extends IterativeRobot {
         starSlave = new Talon(Constants.t4Port);
         
         //Robot Drive
-        d = new RobotDrive(portMaster, portSlave, starMaster, starSlave);
-        d.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-        d.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
-        d.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-        d.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+        driveTrain = new RobotDrive(portMaster, portSlave, starMaster, starSlave);
+        driveTrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        driveTrain.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        driveTrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        driveTrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         
         //Joysticks
-        rs = new Joystick(Constants.rsPort);
-        ls = new Joystick(Constants.lsPort);
+        rightStick = new Joystick(Constants.rsPort);
         
         //Spikes
         /*
@@ -83,47 +78,44 @@ public class RobotTemplate extends IterativeRobot {
         *KOn - Sets both relay outputs on (M+ @ 12V, M- @ 12V).
         */
         //Change to SingleSidedSpikes
-        r5 = new Relay(Constants.r5Port);
-        r6 = new Relay(Constants.r6Port);
-        r7 = new Relay(Constants.r7Port);
-        r8 = new Relay(Constants.r8Port);
+        relay5 = new Relay(Constants.r5Port);
+        relay6 = new Relay(Constants.r6Port);
+        relay7 = new Relay(Constants.r7Port);
+        relay8 = new Relay(Constants.r8Port);
         
         //Even numbered barrels use kForward, Odd numbers use kReverse
         //SingleSideSpikes
-        s1 = new SingleSideSpike(1, r5);
-        s2 = new SingleSideSpike(2, r5);
-        s3 = new SingleSideSpike(3, r6);
-        s4 = new SingleSideSpike(4, r6);
-        s5 = new SingleSideSpike(5, r7);
-        s6 = new SingleSideSpike(6, r7);
-        s7 = new SingleSideSpike(7, r8);
+        spike1 = new SingleSideSpike(1, relay5);
+        spike2 = new SingleSideSpike(2, relay5);
+        spike3 = new SingleSideSpike(3, relay6);
+        spike4 = new SingleSideSpike(4, relay6);
+        spike5 = new SingleSideSpike(5, relay7);
+        spike6 = new SingleSideSpike(6, relay7);
+        spike7 = new SingleSideSpike(7, relay8);
         
         //Barrels
-        b1 = new Barrel(1, s1);
-        b2 = new Barrel(2, s2);
-        b3 = new Barrel(3, s3);
-        b4 = new Barrel(4, s4);
-        b5 = new Barrel(5, s5);
-        b6 = new Barrel(6, s6);
-        b7 = new Barrel(7, s7);
+        barrel1 = new Barrel(1, spike1);
+        barrel2 = new Barrel(2, spike2);
+        barrel3 = new Barrel(3, spike3);
+        barrel4 = new Barrel(4, spike4);
+        barrel5 = new Barrel(5, spike5);
+        barrel6 = new Barrel(6, spike6);
+        barrel7 = new Barrel(7, spike7);
         
-        //Barrel Array
-        barrelList = new Barrel[7];
-        barrelList[0] = b1;
-        barrelList[1] = b2;
-        barrelList[2] = b3;
-        barrelList[3] = b4;
-        barrelList[4] = b5;
-        barrelList[5] = b6;
-        barrelList[6] = b7;
-        
-        //StateMachine
-        stateMachine = new StateMachine(6);
+        //Barrel Queue
+        barrelList = new CircularQueue();
+        barrelList.addData(barrel1);
+        barrelList.addData(barrel2);
+        barrelList.addData(barrel3);
+        barrelList.addData(barrel4);
+        barrelList.addData(barrel5);
+        barrelList.addData(barrel6);
+        barrelList.addData(barrel7);
     }
 
     public void teleopPeriodic() {
         //Runs drive train
-        d.arcadeDrive(rs);
+        driveTrain.arcadeDrive(rightStick);
         
         //Used for debuging/fires single barrel out of order
         debugControl();
@@ -132,24 +124,21 @@ public class RobotTemplate extends IterativeRobot {
         stateBasedControl();
         
         //Resets barrel state
-        if(rs.getRawButton(3)){
+        if(rightStick.getRawButton(3)){
             resetBarrels();
         }
     }
 
-    //Change to use queue instead of whatever this is
     private void stateBasedControl(){
         //Fires next barrel when trigger is pressed
-        if(rs.getRawButton(1) && released){
-            int currentState = stateMachine.getCurrentState();
-            Barrel currentBarrel = barrelList[currentState];
+        if(rightStick.getRawButton(1) && released){
+            Barrel currentBarrel = (Barrel) barrelList.get();
             if(!currentBarrel.isFired()){
-                stateMachine.incrementState();
+                currentBarrel.fire();
             }
-            stateMachine.incrementState();
             released = false;
         }
-        if(!rs.getRawButton(1)){
+        if(!rightStick.getRawButton(1)){
             released = true;
         }
     }
@@ -157,80 +146,78 @@ public class RobotTemplate extends IterativeRobot {
     //Change to use single sided solenoid
     private void debugControl(){
         //Fires barrel based on what button is pressed       
-        if(rs.getRawButton(Constants.rsB1Fire)){
-            r5.set(Relay.Value.kReverse);
+        if(rightStick.getRawButton(Constants.rsB1Fire)){
+            relay5.set(Relay.Value.kReverse);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r5.set(Relay.Value.kOff);
+            relay5.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB2Fire)){
-            r5.set(Relay.Value.kForward);
+        if(rightStick.getRawButton(Constants.rsB2Fire)){
+            relay5.set(Relay.Value.kForward);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r5.set(Relay.Value.kOff);
+            relay5.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB3Fire)){
-            r6.set(Relay.Value.kReverse);
+        if(rightStick.getRawButton(Constants.rsB3Fire)){
+            relay6.set(Relay.Value.kReverse);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r6.set(Relay.Value.kOff);
+            relay6.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB4Fire)){
-            r6.set(Relay.Value.kForward);
+        if(rightStick.getRawButton(Constants.rsB4Fire)){
+            relay6.set(Relay.Value.kForward);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r6.set(Relay.Value.kOff);
+            relay6.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB5Fire)){
-            r7.set(Relay.Value.kReverse);
+        if(rightStick.getRawButton(Constants.rsB5Fire)){
+            relay7.set(Relay.Value.kReverse);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r7.set(Relay.Value.kOff);
+            relay7.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB6Fire)){
-            r7.set(Relay.Value.kForward);
+        if(rightStick.getRawButton(Constants.rsB6Fire)){
+            relay7.set(Relay.Value.kForward);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r7.set(Relay.Value.kOff);
+            relay7.set(Relay.Value.kOff);
         }
-        if(rs.getRawButton(Constants.rsB7Fire)){
-            r8.set(Relay.Value.kReverse);
+        if(rightStick.getRawButton(Constants.rsB7Fire)){
+            relay8.set(Relay.Value.kReverse);
             try {
                 Thread.sleep(Constants.sleepTime);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            r8.set(Relay.Value.kOff);
+            relay8.set(Relay.Value.kOff);
         }
     }
     
     //Resets the state of all barrels
     private void resetBarrels(){
-        b1.setFired(false);
-        b2.setFired(false);
-        b3.setFired(false);
-        b4.setFired(false);
-        b5.setFired(false);
-        b6.setFired(false);
-        b7.setFired(false);
-        stateMachine.setCurrentState(0);
+        barrel1.setFired(false);
+        barrel2.setFired(false);
+        barrel3.setFired(false);
+        barrel4.setFired(false);
+        barrel5.setFired(false);
+        barrel6.setFired(false);
     }
 }
