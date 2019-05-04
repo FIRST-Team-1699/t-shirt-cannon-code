bcb3d11da3ff11c4a284ffbdebc4e9697b0c1b73
Jakob Misbach <jakobmisbach8@gmail.com>
2019-05-03 23:11:16 -0400

    Added messages to make failed tests more readable

--- test-dashboard-client/tst/com/team1699/wrist/WristSimTest.java
+++ test-dashboard-client/tst/com/team1699/wrist/WristSimTest.java
@@ -42,7 +42,7 @@ public class WristSimTest {
     }
 
     void simulateTime(final double voltage, final double time){
-        assertTrue(voltage <= 12.0 && voltage >= -12.0);
+        assertTrue(voltage <= 12.0 && voltage >= -12.0, String.format("System asked for: %f volts which is greater than 12.0", voltage));
         final double kSimTime = 0.0001;
 
         double currentTime = 0.0;
@@ -52,10 +52,10 @@ public class WristSimTest {
             simWrist.velocity += acceleration * kSimTime;
             currentTime += kSimTime;
             if(simWrist.limitTriggered()){
-                assertTrue(simWrist.velocity > -0.051);
+                assertTrue(simWrist.velocity > -0.05, String.format("System running at %f m/s which is less than -0.051", simWrist.velocity));
             }
-            assertTrue(simWrist.position >= WristLoop.kMinHeight - 0.01);
-            assertTrue(simWrist.position <= WristLoop.kMaxHeight + 0.01);
+            assertTrue(simWrist.position >= WristLoop.kMinHeight - 0.01, String.format("System is at %f meters which is less than minimum height of %f", simWrist.position, WristLoop.kMinHeight));
+            assertTrue(simWrist.position <= WristLoop.kMaxHeight + 0.01, String.format("System is at %f meters which is greater than the maximum height of %f", simWrist.position, WristLoop.kMaxHeight));
         }
     }
 }
